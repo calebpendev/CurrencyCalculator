4070bacbbcba09ce03b0d800fc53c5811a617ddd
calebpendev <calebpendev@gmail.com>
2021-08-15 12:50:33 -0700

    update

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CurrencyExchangeService } from '../services/exchange-rates.service'
+import { CurrencyExchangeService } from '../services/exchange-rates.service';
 
 
 @Component({
